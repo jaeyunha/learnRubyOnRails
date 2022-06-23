c41bfaaa116e15f91bf8bb76a944bb9dfe9e940e
Jaeyun Ha <jaeyunha92@gmail.com>
2022-06-23 01:26:31 +0000

    After learning read in CRUD

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,6 +1,6 @@
 class BlogController < ApplicationController
 	def index
-		
+		@posts = Post.all
 	end
 	
 	def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	root :to => "blog#index"
+	post "/create" => "blog#create"
+	
 	get "/home" => "blog#index" #/home
 	get "/test" => "blog#test" #/test
 	get "/result" => "calculator#result"
