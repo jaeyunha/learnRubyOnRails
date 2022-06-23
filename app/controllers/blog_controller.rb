7344e97695815ea78eb64f6e5229d8563d06bcd3
Jaeyun Ha <jaeyunha92@gmail.com>
2022-06-23 21:06:19 +0000

    After learning all about CRUD

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -12,6 +12,26 @@ class BlogController < ApplicationController
 		redirect_to '/'
 	end
 	
+	def update
+		@post = Post.find(params[:postId])
+	end
+	
+	def updateAction
+		@id = params[:id]
+		post = Post.find(@id)
+		post.title = params[:title]
+		post.description = params[:description]
+		post.save
+		redirect_to '/'
+	end
+	
+	def delete
+		Post.destroy(params[:postId])
+		redirect_to '/'
+	end
+
+		
+	
 	def test
 		@condition = true # or false
 		@temp = "안녕하세요!"
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	root :to => "blog#index"
 	post "/create" => "blog#create"
+	get "/update/:postId" => "blog#update"
+	post "/updateAction" => "blog#updateAction"
+	get "/delete/:postId" => "blog#delete"
+
 	
-	get "/home" => "blog#index" #/home
-	get "/test" => "blog#test" #/test
+	
+	get "/home" => "blog#index"
+	get "/test" => "blog#test"
 	get "/result" => "calculator#result"
-	
+	post "/result" => "calculator#result"
+	get "/plus/:num1/:num2" => "calculator#plus"
 end
