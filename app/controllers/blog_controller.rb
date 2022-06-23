4d29b5481a6bc3fd355ede0994427f9c8e4fd81f
Jaeyun Ha <jaeyunha92@gmail.com>
2022-06-23 00:17:05 +0000

    To update

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,6 +3,15 @@ class BlogController < ApplicationController
 		
 	end
 	
+	def create
+		post = Post.new
+		post.title = params[:title]
+		post.description = params[:description]
+		post.save
+		
+		redirect_to '/'
+	end
+	
 	def test
 		@condition = true # or false
 		@temp = "안녕하세요!"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-	root :to => "calculator#main"
+	root :to => "blog#index"
 	get "/home" => "blog#index" #/home
 	get "/test" => "blog#test" #/test
 	get "/result" => "calculator#result"
+	
 end
